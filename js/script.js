fd7665f9fb9663fc0e3b66fad6c4bfde9bea4710
DevClaudioFilho <devclaudiofilh@gmail.com>
2021-02-27 20:44:24 +0000

    frist commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide:"quote",
+    nav:false,
+    auto:true,
+    time:5000
+  });
+  
+  new SimpleSlide({
+    slide:"portifolio",
+    nav:true,
+    auto:true,
+    time:5000
+  });
+}
+if(window.SimpleAnime){
+ new SimpleAnime();
+}
